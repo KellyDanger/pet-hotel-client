8ef5444185c0eeb3e8b04dc021390552456542a6
Stav Kidron <kidronstav@gmail.com>
2020-12-04 13:56:35 -0600

    getting info from DB and displaying on table, post route created and working through postman

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   const [placeholder, setPlaceholder] = useState('Not Connected');
+  // const [newPet, setPet] = useState('No Pet For You');
 
   useEffect(() => {
-    fetch('/hello').then(res => res.json()).then(data => {
+    fetch('/pets').then(res => res.json()).then(data => {
       setPlaceholder(data.result);
-      console.log(data.result[0]);
     });
   }, []);
 
@@ -16,7 +16,34 @@ function App() {
       {/* <header className="App-header">
         <p>Flask says {placeholder[0][1]}</p>
       </header> */}
-      <h2>Pets</h2>
+      <h1>Submit Your Pet</h1>
+
+      <form className="form">
+
+        <label htmlFor="nameInput">Name:</label>
+        <input id="nameInput"/>
+        <br/>
+        <label htmlFor="ownerInput">Owner ID:</label>
+        <input id="ownerInput" type="number"/>
+        <br/>
+        <label htmlFor="breedInput">Breed:</label>
+        <input id="breedInput"/>
+        <br/>
+        <label htmlFor="colorInput">Color:</label>
+        <input id="colorInput"/>
+        <br/>
+        <label htmlFor="checkInInput">Checked In Status:</label>
+        <input id="checkInInput" type="boolean" defaultValue="true"/>
+        <br/>
+        <label htmlFor="dateInput">Check In Date:</label>
+        <input id="dateInput" type="date"/>
+
+        <button>Submit</button>
+
+      </form>
+
+      <h2>Pets in Zootopia Hotel</h2>
+
       <table>
         <thead>
           <tr>
@@ -35,9 +62,9 @@ function App() {
             <td>{placeholder[0][3]}</td>
             <td>{placeholder[0][4]}</td>
             <td>{placeholder[0][5]?
-            "Purple"
+            "Yes"
             :
-            "Red"}</td>
+            "No"}</td>
             <td>{placeholder[0][6]}</td>
           </tr>
         </tbody>
